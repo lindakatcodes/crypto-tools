829d32e98a575798ddfbac3808572a9e724ce7ae
LindaKat <lindakt16@gmail.com>
2021-05-29 21:49:28 -0500

    pausing to learn better ways to access data

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,12 +6,5 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-  data: Object = this.fetchKeyData();
-
-  async fetchKeyData() {
-    return await fetch('./../../../netlify/functions/getKeyData').then(res => res.json)
-      .then(result => result)
-      .catch(err => console.error(err));
-  }
-
+ 
 }
